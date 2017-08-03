38777eaffcc021e5704c310c06d04c0eec785814
Mike Cornwell <michael.a.cornwell@gmail.com>
2017-08-02 23:30:30 -0500

    Created basic initialization script and the development script.

---
+++ dev.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo Updating package manager
+sudo apt-get update
+
+echo Installing git
+sudo apt-get install -y git
+
+echo Installing Python3
+sudo apt-get install -y python3
+
+echo Installing Python3-pip
+sudo apt-get install -y python3-pip
+
